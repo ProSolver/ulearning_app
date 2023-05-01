51ab517fe39ddd2d680312711ecc406a12007358
ProSolver-org <poralekha@prosolver.org>
2023-05-01 18:40:11 +0600

    o=> Section 4: WelcomeScreen

---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
