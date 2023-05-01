be9494ee1bb34f033de79c1a9109d4bc87d8ab52
ProSolver-org <poralekha@prosolver.org>
2023-05-01 17:51:13 +0600

    o=> 7. Importance of BlocBuilder widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ulearning_app/app_blocs.dart';
+import 'package:ulearning_app/app_events.dart';
 import 'package:ulearning_app/app_states.dart';
 
 void main() {
@@ -57,34 +58,31 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: BlocBuilder<AppBlocs, AppStates>(
-          builder: (context, state) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '${BlocProvider.of<AppBlocs>(context).state.counter}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            );
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${BlocProvider.of<AppBlocs>(context).state.counter}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           FloatingActionButton(
-            onPressed: _incrementCounter,
+            onPressed: () =>
+                BlocProvider.of<AppBlocs>(context).add(Increment()),
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
           FloatingActionButton(
             onPressed: _decrementCounter,
-            tooltip: 'Increment',
+            tooltip: 'Decrement',
             child: const Icon(Icons.remove),
           ),
         ],
