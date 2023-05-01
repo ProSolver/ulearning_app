07407e7e0b2a8cd23da64332270272ae64f5f4f6
ProSolver-org <poralekha@prosolver.org>
2023-05-01 20:24:54 +0600

    o=> Lecture thumbnail 6:35 / 6:51 12. Welcome screen and reusable widget ui part 4

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -22,64 +22,29 @@ class _WelcomeState extends State<Welcome> {
             children: [
               PageView(
                 children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 345.w,
-                        height: 345.h,
-                        child: const Text("Image one"),
-                      ),
-                      Container(
-                        child: Text(
-                          "First See Learning",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 375.w,
-                        padding: EdgeInsets.only(left: 30.w, right: 30.w),
-                        child: Text(
-                          "Forget about a for of paper all knowledge in one learning",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(
-                            top: 100.h, left: 25.w, right: 25.w),
-                        width: 325.w,
-                        height: 50.h,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.all(Radius.circular(15.w)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.1),
-                              spreadRadius: 1,
-                              blurRadius: 2,
-                              offset: const Offset(0, 1),
-                            ),
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "next",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                  _page(
+                    1,
+                    context,
+                    "next",
+                    "First See Learning",
+                    "Forget about a for of paper all knowledge in one learning",
+                    "imagePath",
+                  ),
+                  _page(
+                    2,
+                    context,
+                    "next",
+                    "Connect With Everyone",
+                    "Always keep in touch with your tutor & friends. Let's get connected",
+                    "imagePath",
+                  ),
+                  _page(
+                    3,
+                    context,
+                    "get started",
+                    "Always fascinated Learning",
+                    "Anywhere, anytime. The time is at your discretion so study whenever you want",
+                    "imagePath",
                   ),
                 ],
               ),
@@ -87,6 +52,68 @@ class _WelcomeState extends State<Welcome> {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _page(int index, BuildContext context, String buttonName, String title,
+      String subTitle, String imagePath) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 345.w,
+          height: 345.h,
+          child: const Text("Image one"),
+        ),
+        Container(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24.sp,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        Container(
+          width: 375.w,
+          padding: EdgeInsets.only(left: 30.w, right: 30.w),
+          child: Text(
+            subTitle,
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.5),
+              fontSize: 14.sp,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+          width: 325.w,
+          height: 50.h,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(Radius.circular(15.w)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Text(
+              buttonName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
