0bf4f7aa8fcc2cca26f0762c9c5d3b7e7baf752f
ProSolver-org <poralekha@prosolver.org>
2023-05-01 19:51:53 +0600

    o=> 0:00 / 0:00 11. Welcome screen and work on the next button ui part 3 2

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -40,12 +40,42 @@ class _WelcomeState extends State<Welcome> {
                         ),
                       ),
                       Container(
+                        width: 375.w,
+                        padding: EdgeInsets.only(left: 30.w, right: 30.w),
                         child: Text(
                           "Forget about a for of paper all knowledge in one learning",
                           style: TextStyle(
                             color: Colors.black.withOpacity(0.5),
                             fontSize: 14.sp,
                             fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: 100.h, left: 25.w, right: 25.w),
+                        width: 325.w,
+                        height: 50.h,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.1),
+                              spreadRadius: 1,
+                              blurRadius: 2,
+                              offset: const Offset(0, 1),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Text(
+                            "next",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.normal,
+                            ),
                           ),
                         ),
                       ),
