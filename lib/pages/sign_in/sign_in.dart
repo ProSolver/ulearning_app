31521fe05a631f9a58294e993facc5d93e70b2d7
ProSolver-org <poralekha@prosolver.org>
2023-05-02 12:40:05 +0600

    o=> 22. Sign in screen and app bar part 1_2

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/pages/sign_in/widgets/sign_in_widget.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -17,22 +18,7 @@ class _SignInState extends State<SignIn> {
       child: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
-          appBar: AppBar(
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(1.0),
-              child: Container(
-                color: Colors.grey.withOpacity(0.5),
-                height: 1.0,
-              ),
-            ),
-            title: Text(
-              "Log In",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.normal),
-            ),
-          ),
+          appBar: buildAppBar(),
         ),
       ),
     );
---
+++ lib/pages/sign_in/widgets/sign_in_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+AppBar buildAppBar() {
+  return AppBar(
+    bottom: PreferredSize(
+      preferredSize: const Size.fromHeight(1.0),
+      child: Container(
+        color: Colors.grey.withOpacity(0.5),
+        height: 1.0,
+      ),
+    ),
+    title: Text(
+      "Log In",
+      style: TextStyle(
+          color: Colors.black, fontSize: 16.sp, fontWeight: FontWeight.normal),
+    ),
+  );
+}
